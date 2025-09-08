6360438ff869eb91e48987ebad1cf889e51c4e29
Jingwei <a0979580915@gmail.com>
2025-09-08 14:40:05 +0800

    change metadata table and add backup

--- play_rosbag_with_rmw_zenoh.sh
+++ play_rosbag_with_rmw_zenoh.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-bag_path=rosbag/carla_rosbag_one_camera
+if [ -z "$1" ]; then
+  echo "Usage: $0 <bag_path>"
+  exit 1
+fi
+
+bag_path=$1
 
 topics=$(ros2 bag info $bag_path | grep -E 'Topic:\s+/(vehicle|system)[^ ]*' | awk '{print $2}' | tr '\n' ' ')
 
@@ -8,5 +13,5 @@ sudo true
 
 parallel --lb ::: \
   "sudo -E env "PYTHONPATH=$PYTHONPATH" "LD_LIBRARY_PATH=$LD_LIBRARY_PATH" "PATH=$PATH" "USER=$USER" bash -c 'ros2 run rmw_zenoh_cpp rmw_zenohd'" \
-  "sudo -E env "PYTHONPATH=$PYTHONPATH" "LD_LIBRARY_PATH=$LD_LIBRARY_PATH" "PATH=$PATH" "USER=$USER" bash -c \"ros2 bag play $bag_path --topics $topics\"" \
+  "sudo -E env "PYTHONPATH=$PYTHONPATH" "LD_LIBRARY_PATH=$LD_LIBRARY_PATH" "PATH=$PATH" "USER=$USER" bash -c \"ros2 bag play $bag_path --topics $topics -l\"" \
   "sudo -E env "PYTHONPATH=$PYTHONPATH" "LD_LIBRARY_PATH=$LD_LIBRARY_PATH" "PATH=$PATH" "USER=$USER" bash -c 'ros2 run subscriber subscriber'"
