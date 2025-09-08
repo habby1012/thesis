3374bd81e1b660ca4cda89ee7abfd9596a566234
Jingwei <a0979580915@gmail.com>
2025-09-08 21:26:08 +0800

    fix

--- exp/case1/one_camera_one_lidar/config.sh
+++ exp/case1/one_camera_one_lidar/config.sh
@@ -1,9 +1,8 @@
 # set vlan
 sudo ip link set dev vlan2 type vlan egress 0:0 1:1 2:2 3:3 4:4 5:5 6:6 7:7
-sudo tc qdisc del dev enp4s0 root
 
 # replace metadata table
-cp ../../src/subscriber/subscriber/config/backup/INTER_carla_one_camera_DEFAULT.csv ../../src/subscriber/subscriber/config/INTER_carla_one_camera.csv
+cp /home/newslab/repos/thesis/src/subscriber/subscriber/config/backup/INTER_carla_one_camera_DEFAULT.csv /home/newslab/repos/thesis/src/subscriber/subscriber/config/INTER_carla_one_camera.csv
 
 # config tc qdisc
 sudo tc qdisc del dev enp4s0 root
