686915a3ca1217e83b4ae59fe50f72f15add0b7e
Jingwei <a0979580915@gmail.com>
2025-09-16 13:26:31 +0800

    add case3

--- exp/case2/one_camera_one_lidar/config.sh
+++ exp/case2/one_camera_one_lidar/config.sh
@@ -6,8 +6,8 @@ sudo tc qdisc del dev enp4s0 root
 sudo tc qdisc replace dev enp4s0 root mqprio   num_tc 1 map 0 0 0 0 0 0 0 0   queues 1@0 hw 0
 tc qdisc
 
-# Set Vlan. This is to ensure host's hardware queues map to corresponding switch's queues.
-sudo ip link set dev vlan2 type vlan egress 0:0 1:1 2:2 3:3 4:4 5:5 6:6 7:7
+# Set Vlan. This is to ensure host's hardware queues map to one switch's queue.
+sudo ip link set dev vlan2 type vlan egress 0:2 1:2 2:2 3:2 4:2 5:2 6:2 7:2
 
 # test ptp
 sudo systemctl status ptp4l
